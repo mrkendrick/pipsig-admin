7538ff2c8fa133a0ef030f47dac445a71d25b367
Charles Duruaku <charlesrichon@gmail.com>
2022-03-27 21:35:22 +0000

    changed storage key

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -74,5 +74,5 @@ class Helper {
   }
 }
 
-const HelperServices = new Helper('pipsig_token', 'pipsigxRefCode')
+const HelperServices = new Helper('pipsig_admin_token', 'pipsigxRefCode')
 export default HelperServices
